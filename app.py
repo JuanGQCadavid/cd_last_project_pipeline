e5064afce4b0f2e7bd053ec74c1df2551dc7631e
JuanGQCadavid <juan.gonzalo@comono.co>
2020-09-25 00:41:19 -0500

    Adding lambda with apigateway

--- app.py
+++ app.py
@@ -2,11 +2,9 @@
 
 from aws_cdk import core
 
-from pipeline.pipeline_stack import PipelineStack
 from pipeline.pipeline_inner_stack import PipelineInnerStack
 
 app = core.App()
-PipelineStack(app, "pipeline")
 PipelineInnerStack(app,'PipelineInnerStack', env={
     'region': 'us-east-1'
 })
---
+++ pipeline/webservice_stage.py
@@ -0,0 +1,10 @@
+from aws_cdk import core
+from .pipeline_stack import PipelineStack
+
+class WebServiceStage(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs):
+        super().__init__(scope,id,**kwargs)
+
+        service = PipelineStack(self,"WebService")
+
+        self.url_output = service.url_output
